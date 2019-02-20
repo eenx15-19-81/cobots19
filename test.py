6cedc452be868a89484def68740f19402e9ff926
Victor Hanson <vectoralarm@gmail.com>
2019-02-20 17:38:25 +0100

    Basic gripper support

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from trajectory_msgs.msg import *
 from sensor_msgs.msg import JointState
 import math
 from std_msgs.msg import String
-#from Robotiq2FGripper_robot_output.msg import *
+from robotiq_2f_gripper_control.msg import _Robotiq2FGripper_robot_output as outputMsg
 
 
 class test():
@@ -19,6 +19,7 @@ class test():
 	def __init__(self):
 		print("Init talk")
 		self.initTalker()
+		self.initTalkerGripper()
 		print("init sub")
 		self.initSubscriber()
 		time.sleep(3)
@@ -29,11 +30,21 @@ class test():
 		joint_pose2=[0.995, -1, -2.013, -2.652, -0.140, -0.532]
 		pos1 = "movej(" + str(joint_home) + ", a=" + str(a) + ", v=" + str(v) + ", t=" + str(0) + ", r=" + str(0) + ")"
 		pos2 = "movej(" + str(joint_pose2) + ", a=" + str(a) + ", v=" + str(v) + ", t=" + str(0) + ", r=" + str(0) + ")"
+
+		closeGripper = outputMsg.Robotiq2FGripper_robot_output(rACT = 1, rGTO = 1, rATR = 0, rPR = 255, rSP = 255, rFR = 25)
+		openGripper = outputMsg.Robotiq2FGripper_robot_output(rACT = 1, rGTO = 1, rATR = 0, rPR = 0, rSP = 255, rFR = 25)
 		
+		# Open gripper: rACT = 1, rGTO = 1, rATR = 0, rPR = 0, rSP = 255, rFR = 25
+		# Close gripper: rACT = 1, rGTO = 1, rATR = 0, rPR = 255, rSP = 255, rFR = 25
+
 		while not rospy.is_shutdown():
 			print("Going to Home")
 			self.talk(pos1)
 			self.waitForMove(0.001, joint_home)
+			self.talkGripper(closeGripper)
+			time.sleep(1)
+			self.talkGripper(openGripper)
+			time.sleep(1)
 			print("Going to Pos")
 			self.talk(pos2)
 			self.waitForMove(0.001, joint_pose2)
@@ -45,6 +56,8 @@ class test():
 		self.urPublisher=rospy.Publisher('/ur_driver/URScript',String,queue_size=10)
 		rospy.init_node('talker',anonymous=True)
 		
+	def initTalkerGripper(self):
+		self.gripperPub = rospy.Publisher('/Robotiq2FGripperRobotOutput',outputMsg.Robotiq2FGripper_robot_output , queue_size=10)
 	
 	def initSubscriber(self):
 		#rospy.init_node('listener',anonymous=True)
@@ -78,6 +91,8 @@ class test():
 		rospy.loginfo(msg)
 		self.urPublisher.publish(msg)									#Publish hello_str to node
 		
+	def talkGripper(self, msg):
+		self.gripperPub.publish(msg)
 try:
 	test()
 except rospy.ROSInterruptException:
