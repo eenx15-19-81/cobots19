8bdbd43ecc2adbbe5e3dedd751127b8cc8a817ad
emquist <emil_96_malmquist@hotmail.com>
2019-02-21 14:27:42 +0100

    Cleaned code  updated with comments

--- test.py
+++ test.py
@@ -8,32 +8,37 @@ from trajectory_msgs.msg import *
 from sensor_msgs.msg import JointState
 import math
 from std_msgs.msg import String
-#from Robotiq2FGripper_robot_output.msg import *
+from robotiq_2f_gripper_control.msg import _Robotiq2FGripper_robot_output as outputMsg
 
 
 class test():
-	JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
-               'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+	
 	jointStates = []
 			   
 	def __init__(self):
-		print("Init talk")
-		self.initTalker()
-		print("init sub")
-		self.initSubscriber()
-		time.sleep(3)
-		print("Done with init.")
-		a=0.1
-		v=0.3
+		self.initialize()
+		acceleration=0.1
+		velocity=0.3
 		joint_home=[0,-1.5,0,-1.5, 0, 0]
 		joint_pose2=[0.995, -1, -2.013, -2.652, -0.140, -0.532]
-		pos1 = "movej(" + str(joint_home) + ", a=" + str(a) + ", v=" + str(v) + ", t=" + str(0) + ", r=" + str(0) + ")"
-		pos2 = "movej(" + str(joint_pose2) + ", a=" + str(a) + ", v=" + str(v) + ", t=" + str(0) + ", r=" + str(0) + ")"
+		pos1 = "movej(" + str(joint_home) + ", a=" + str(acceleration) + ", v=" + str(velocity) + ", t=" + str(0) + ", r=" + str(0) + ")"
+		pos2 = "movej(" + str(joint_pose2) + ", a=" + str(acceleration) + ", v=" + str(velocity) + ", t=" + str(0) + ", r=" + str(0) + ")"
+
+		closeGripper = outputMsg.Robotiq2FGripper_robot_output(rACT = 1, rGTO = 1, rATR = 0, rPR = 255, rSP = 255, rFR = 25)
+		openGripper = outputMsg.Robotiq2FGripper_robot_output(rACT = 1, rGTO = 1, rATR = 0, rPR = 0, rSP = 255, rFR = 25)
 		
+		# Open gripper: rACT = 1, rGTO = 1, rATR = 0, rPR = 0, rSP = 255, rFR = 25
+		# Close gripper: rACT = 1, rGTO = 1, rATR = 0, rPR = 255, rSP = 255, rFR = 25
+	
+		# Main working loop
 		while not rospy.is_shutdown():
 			print("Going to Home")
 			self.talk(pos1)
 			self.waitForMove(0.001, joint_home)
+			self.talkGripper(closeGripper)
+			time.sleep(1)
+			self.talkGripper(openGripper)
+			time.sleep(1)
 			print("Going to Pos")
 			self.talk(pos2)
 			self.waitForMove(0.001, joint_pose2)
@@ -45,12 +50,26 @@ class test():
 		self.urPublisher=rospy.Publisher('/ur_driver/URScript',String,queue_size=10)
 		rospy.init_node('talker',anonymous=True)
 		
+	def initTalkerGripper(self):
+		self.gripperPub = rospy.Publisher('/Robotiq2FGripperRobotOutput',outputMsg.Robotiq2FGripper_robot_output , queue_size=10)
 	
 	def initSubscriber(self):
-		#rospy.init_node('listener',anonymous=True)
 		rospy.Subscriber("/joint_states",JointState,self.callback)
-		#rospy.spin()
-
+		
+	def initialize(self):
+		print "Initializing Talkers and Subscribers."
+		self.initTalker()
+		self.initTalkerGripper()
+		self.initSubscriber()
+		time.sleep(3)
+		print("Done with initialization.")
+		
+		
+	#####################
+	##### Functions #####
+	#####################
+	
+	# Wait for current move to be done [Margin in radians, Desired position as 6 floats of radians]
 	def waitForMove(self, margin, desiredPosition):
 		print("Waiting for move...")
 		done = False
@@ -61,23 +80,25 @@ class test():
 				else:
 					done = True
 				#print str(x) + ": " + str(done)
-				print str(desiredPosition[x]) + " | " + str(self.jointStates[x]) 
+				print str(desiredPosition[x]) + " | " + str(self.jointStates[x])
 			if(done == True):
 				break
 			time.sleep(0.25)
 			print("______________________")
-
 	
+		
+	# Callback from the URSubscriber updating jointstates with current position
 	def callback(self,data):
-		print "i Heard: " + str(data.position)
 		self.jointStates = data.position
-		#time.sleep(0.25)
-
 	
+	# Publish the msg to UR node
 	def talk(self,msg):			
 		rospy.loginfo(msg)
-		self.urPublisher.publish(msg)									#Publish hello_str to node
-		
+		self.urPublisher.publish(msg)
+	
+	# Publish the msg to Gripper node
+	def talkGripper(self, msg):
+		self.gripperPub.publish(msg)
 try:
 	test()
 except rospy.ROSInterruptException:
