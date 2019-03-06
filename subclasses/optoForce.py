234b79ba1dcf0a486bcb0cf813b469e35584a0bc
emquist <emilmal@student.chalmers.se>
2019-03-06 11:26:47 +0100

    Wroking gripping and proper force control

--- subclasses/optoForce.py
+++ subclasses/optoForce.py
@@ -1,19 +1,35 @@
 import numpy as np
 import math
+import time
+
 
 from geometry_msgs.msg import TwistStamped, WrenchStamped
 
 class optoForce():
     curForce=[]
     curTorque=[]
-    def __init__(self):
+    def __init__(self,tf,rospy):
+        self.tf = tf
+        self.rospy = rospy
         print("Initialized optoForce class")
+        self.listener = tf.TransformListener()
 
     def setCurrentForce(self, curForce):
         self.curForce=curForce
 
     def setCurrentTorque(self,curTorque):
         self.curTorque=curTorque
+
+    def transformMatrix(self, frame1, frame2):
+        self.listener.waitForTransform( '/'+frame1, '/'+frame2, self.rospy.Time(0),self.rospy.Duration(1))
+        (trans,rot) = self.listener.lookupTransform('/'+frame1, '/'+frame2, self.rospy.Time(0))
+        return self.listener.fromTranslationRotation(trans, rot)
+
+    def convertFrame(self, velocity):
+        rotationMatrix = self.transformMatrix('base','tool0_controller')
+        linearVelocity = np.matmul(rotationMatrix[0:3,0:3], velocity[0:3]) 
+        velocity = np.concatenate((linearVelocity, np.array([0,0,0]))) 
+        return velocity
 
     def getSpeedl(self, acceleration = 0.3,time=0.5):
         velocity=self.forceControl()
@@ -30,17 +46,12 @@ class optoForce():
             velocity = np.array([0,0,0])
         desiredVelocity=np.concatenate((velocity,np.array([0.0,0.0,0.0])))
         desiredVelocity =kd_inv * desiredVelocity
-        #TODO rotation_matrix
-        return desiredVelocity
+        velocity = self.convertFrame(desiredVelocity)
+        return velocity
 
     def withinDeadBand(self):
 
         if(math.sqrt(math.pow(self.curForce[0],2)+math.pow(self.curForce[1],2)+math.pow(self.curForce[2],2))>3):
             return True
         return False
-    
-
-        
-        
-
 
