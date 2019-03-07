4ab33702d4ebc481beb3397db1013674071724b3
Victor Hanson <vectoralarm@gmail.com>
2019-03-07 19:59:52 +0100

    Orientation control working ok, but will not compensate for weight of tool when tilted too far from original orientation

--- subclasses/optoForce.py
+++ subclasses/optoForce.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python
 import numpy as np
 import math
 import time
@@ -8,6 +9,11 @@ from geometry_msgs.msg import TwistStamped, WrenchStamped
 class optoForce():
     curForce=[]
     curTorque=[]
+
+    # Increase if the robot "wanders" when in forcecontrol
+    deadbandForce=10
+    deadbandTorque=2.0
+
     def __init__(self,tf,rospy):
         self.forceError=[0,0,0]
         self.forceReference=[0,0,0]
@@ -36,42 +42,42 @@ class optoForce():
         velocity = np.concatenate((linearVelocity, angularVelocity)) 
         return velocity
 
-    def getSpeedl(self, acceleration = 0.3,time=0.5):
+    def getSpeedl(self, acceleration = 0.6, rotAcceleration=1.2, time=0.2):
         velocity=self.forceControl()
-        command = "speedl(" + np.array2string(velocity, precision= 3, separator=',') +","+ \
-        str(acceleration) + "," + str(time) + ")" #0.3,0.2
-        #rospy.loginfo(velocity)
+        command = "speedl(" + np.array2string(velocity, precision= 3, separator=',') +", "+ \
+        str(acceleration) + ", " + str(time) + ", " + rotAcceleration +")" 
+        self.rospy.loginfo(command)
         return command
 
-    def forceControl(self,kd_inv=0.005):
-        if  self.withinDeadBandForce():
+    def forceControl(self, kp_force=0.005, kp_torque=0.2):
+        if not self.withinDeadBandForce():
             velocity = np.array(self.curForce)
-            
-            #TODO selction_vector
+            velocity = velocity - self.deadbandForce    # Start from zero
         else:
-            velocity = np.array([0,0,0])
-        if self.withinDeadBandTorque():
-            torque= np.array(self.curTorque)
+            velocity = np.array([0.0,0.0,0.0])
+        if not self.withinDeadBandTorque():
+            torque = np.array(self.curTorque)
+            torque = torque - self.deadbandTorque   # Start from zero
         else:
             torque = np.array([0.0,0.0,0.0])
-        velocity =kd_inv * velocity
-        desiredVelocity=np.concatenate((velocity,0.2*torque))
+        #TODO selction_vector
+        velocity = kp_force * velocity
+        torque = kp_torque * torque
+        desiredVelocity=np.concatenate((velocity,torque))
         velocity = self.convertFrame(desiredVelocity)
         return velocity
 
     def withinDeadBandForce(self):
-
-        if(math.sqrt(math.pow(self.curForce[0],2)+math.pow(self.curForce[1],2)+math.pow(self.curForce[2],2))>3):
+        if(math.sqrt(math.pow(self.curForce[0],2)+math.pow(self.curForce[1],2)+math.pow(self.curForce[2],2))<self.deadbandForce):
             return True
         return False
 
     def withinDeadBandTorque(self):
-
-        if(math.sqrt(math.pow(self.curTorque[0],2)+math.pow(self.curTorque[1],2)+math.pow(self.curTorque[2],2))>3):
+        if(math.sqrt(math.pow(self.curTorque[0],2)+math.pow(self.curTorque[1],2)+math.pow(self.curTorque[2],2))<self.deadbandTorque):
             return True
         return False
 
-    def forceRegulator(self,force):#ej fungerande än
+    def forceRegulator(self,force):
         Kp=2
         Ki=5
         Kd=1
