d91aeb4cc51398cb04fbec7da6ebe51b4bbc721b
samsjos <samsjos@student.chalmers.se>
2019-03-13 16:38:20 +0100

    calculates with the weight of the gripper

--- subclasses/optoForce.py
+++ subclasses/optoForce.py
@@ -11,9 +11,9 @@ class optoForce():
     curTorque=[]
 
     # Increase if the robot "wanders" when in forcecontrol
-    deadbandForce=3
-    deadbandTorque=0.5
-
+    deadbandForce=0.03
+    deadbandTorque=0.2
+    deadbandVelocity=0.2
     def __init__(self,tf,rospy):
         self.forceError=[0,0,0]
         self.forceReference=[0,0,0]
@@ -45,38 +45,59 @@ class optoForce():
         return velocity
 
     ## Returns a speedl command based on forces read by sensor. This can later be sent to the robots URScript channel. See URScript docs for more information.
-    # rotAcceleration: a separate acceleration value used only for orientation changes (for rx, ry and rz). Unit is rad/s². 
+    # rotAcceleration: a separate acceleration value used only for orientation changes (for rx, ry and rz). Unit is rad/(s*s). 
     # Generally higher than the normal acceleration for better "feel" and less delay.
     # time: maximum amount of time the robot will spend running the command. 
-    def getSpeedl(self, acceleration = 0.6, rotAcceleration=1.2, time=0.2):
+    def getSpeedl(self, acceleration = 0.9, rotAcceleration=1.2, time=0.05):
         velocity=self.forceControl()
-        command = "speedl(" + np.array2string(velocity, precision= 3, separator=',') +", "+ \
+        command = "speedl(" + np.array2string(velocity, precision= 5, separator=',') +", "+ \
         str(acceleration) + ", " + str(time) + ", " + str(rotAcceleration) +")" 
         self.rospy.loginfo(command)
         return command
 
     ## Returns the desired tool velocites in vector form (x, y, z, rx, ry, rz) based on force and torque readings.
     # kp_force and kp_torque can be increased for higher sensitivity and lowered for less sensitivity.
-    def forceControl(self, kp_force=0.01, kp_torque=0.2):
+    def forceControl(self, kp_force=0.02, kp_torque=0.4):
         force = np.array(self.curForce)
         torque = np.array(self.curTorque)
         #TODO selction_vector
         force = kp_force * force
         torque = kp_torque * torque
         velocity = np.concatenate([force, torque])
-        velocity = np.multiply(velocity, np.array(self.getDeadbandVector()))
-        velocity = self.convertFrame(velocity)
-        return velocity
+        rotationMatrix = self.transformMatrix('tool0_controller','base')
 
+        gravitationForce = np.matmul(rotationMatrix[0:3,0:3], [0,0,-0.9*9.82])
+        length = 1/(9.82 * 1.8)
+        Tx = kp_torque * length * gravitationForce[0]
+        Ty = kp_torque * length * gravitationForce[1]
+
+        velocity=np.subtract(velocity,[0,0,-kp_force*0.9*9.82,Tx,Ty,0])
+        velocity = self.convertFrame(velocity)
+        velocity=np.subtract(velocity,[0,0,-kp_force*0.9*9.82,0,0,0])
+        print velocity 
+        velocity=self.checkInDeadband(velocity)
+        return velocity
+    def checkInDeadband(self,velocity):
+        for x in range(3):
+            if abs(velocity[x]) < self.deadbandForce:
+                velocity[x]=0
+            else:
+                velocity[x]=velocity[x]-np.sign(velocity[x])*self.deadbandForce
+        for x in range(3):
+            if abs(velocity[x+3]) < self.deadbandTorque:
+                velocity[x+3]=0
+            else:
+                velocity[x+3]=velocity[x+3]-np.sign(velocity[x+3])*self.deadbandTorque
+        return velocity
     ## Returns a selection vector used for deciding which axises we should move along or rotate around
     # based on if the force/torque in that axis is high enough to overcome the deadband.
-    def getDeadbandVector(self):
+    def getDeadbandVector(self, velocity):
         activeDirections = [0,0,0,0,0,0]
         for x in range(3):
-            if abs(self.curForce[x]) > self.deadbandForce:
+            if abs(velocity[x]) > self.deadbandForce:
                 activeDirections[x] = 1
         for x in range(3):
-            if abs(self.curTorque[x]) > self.deadbandTorque:
+            if abs(velocity[x+3]) > self.deadbandTorque:
                 activeDirections[x+3] = 1      
         return activeDirections
 
