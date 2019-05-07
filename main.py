a4784bc18f2244d79cdd8dd4560e58ee14427f1d
samsjos <samsjos@student.chalmers.se>
2019-05-07 17:12:57 +0200

    changed where save to file is

--- main.py
+++ main.py
@@ -160,15 +160,8 @@ class main():
 		self.o.averageForceMatrix[0].append(data.wrench.force.x)
 		self.o.averageForceMatrix[1].append(data.wrench.force.y)
 		self.o.averageForceMatrix[2].append(data.wrench.force.z)
-		with open("forceSensorData.txt", "a+") as filehandle:  
-			filehandle.write('%s\n' % data.wrench.force.x)
-			filehandle.write('%s\n' % data.wrench.force.y)
-			filehandle.write('%s\n' % data.wrench.force.z)
 		self.o.setCurrentForce([self.o.averageOfList(self.o.averageForceMatrix[0]),self.o.averageOfList(self.o.averageForceMatrix[1]), self.o.averageOfList(self.o.averageForceMatrix[2])])
 		self.o.setCurrentTorque([data.wrench.torque.x, data.wrench.torque.y, data.wrench.torque.z])
-		with open("compensatedData.txt", "a+") as filehandle:  
-			for listitem in self.o.curForce:
-				filehandle.write('%s\n' % listitem)
 			
 
 	# Callback from the gripper with the pressure that it is applying and updates the gripper with the current pressure.
