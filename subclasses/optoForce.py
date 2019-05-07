8b663a460544b03b6321c1057211e275de608746
emquist <emil_96_malmquist@hotmail.com>
2019-05-07 12:36:22 +0200

    new data

--- subclasses/optoForce.py
+++ subclasses/optoForce.py
@@ -29,7 +29,7 @@ class optoForce():
     maxVelocity = 0.3
     maxTorqueVelocity = 0.5
 
-    averagingNo = 30
+    averagingNo = 100
 
 	## Calculate average of a list
     def averageOfList(self,listOfNum): 
