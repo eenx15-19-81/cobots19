0d6229a80e1fedf46eec5e1d9a47ea35d7e1acbe
emquist <emil_96_malmquist@hotmail.com>
2019-05-11 10:48:06 +0200

    now works properly i think

--- subclasses/optoForce.py
+++ subclasses/optoForce.py
@@ -30,7 +30,7 @@ class optoForce():
     maxVelocity = 0.3
     maxTorqueVelocity = 0.5
 
-    averagingNo = 100
+    averagingNo = 1
 
     def __init__(self,tf,rospy):
         self.averageForceMatrix=[[0]*self.averagingNo,[0]*self.averagingNo,[0]*self.averagingNo]
